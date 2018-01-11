b823607145ff1f80ad6db6c7c99505c7a6eb0a66
Fredidennis <fredidennis@gmail.com>
2018-01-11 17:49:15 +0300

    Update dbConnect.php

--- Server Files/dbConnect.php
+++ Server Files/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
- define('HOST','localhost');
- define('USER','id995131_fred');
- define('PASS','fredidennis');
- define('DB','id995131_uploads');
+ define('HOST','127.0.0.1');
+ define('USER','root');
+ define('PASS','');
+ define('DB','tailorscom_db');
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
